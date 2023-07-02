b4d6fac6d008d53eb11ea4933f7d5d087bc818a9
Лилит <@LAPTOP-3EJS7JBS>
2023-07-02 09:53:14 +0300

    Добавьте файлы проекта.

---
+++ HW_tema18_1/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] arr = { "уже", "идет", "второй", "месяц", "лета"};
+string my_word;
+bool isTrue = true;
+string choise;
+
+while (isTrue)
+{
+    Console.Write("Введите ваше слово: ");
+    my_word = Console.ReadLine();
+
+    for(int i=0; i<arr.Length; i++)
+    {
+        if (arr[i] == my_word)
+        {
+            arr[i] = "скрыто";
+        }
+        else Console.WriteLine("совпадений не найдено");
+    }
+
+    Console.Write("Далее/Конец? ");
+    choise = Console.ReadLine();
+    if(choise == "конец")
+    {
+        isTrue = false;
+        for(int i=0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i]+" ");
+        }
+    }
+    else if(choise=="далее") isTrue = true;
+}
